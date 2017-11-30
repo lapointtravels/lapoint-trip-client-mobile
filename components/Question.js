5c6b093ace5a1df1fab7c74e04507a80cd3a0063
Anders Carlsson <andersc@gmail.com>
2017-11-30 16:49:50 +0530

    Fix writing questions to Firestore.

--- components/Question.js
+++ components/Question.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View, TextInput } from 'react-native';
 import { db } from '../config/constants';
 
 export default class Question extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      isEditing: true,
       text: ""
     }
-    this.toggleEdit = this.toggleEdit.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  toggleEdit() {
-    this.setState({ isEditing: !this.state.isEditing })
   }
 
   saveQuestion = (e) => {
+    _this = this;
     db.collection("feedbackQuestions").add({
       text: this.state.text
     })
     .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
+      _this.setState({text: ""})
     })
     .catch(function(error) {
       console.error("Error adding document: ", error);
@@ -30,28 +25,19 @@ export default class Question extends React.Component {
   }
 
   handleChangeText = (text) => {
-    e.preventDefault()
-    this.setState({
-      text: text
-    })
+    this.setState({text: text});
   }
 
   render () {
-    if(this.state.isEditing) {
-      return (
-        <View>
-          <Text>Text</Text>
-          <TextInput 
-            onChangeText={this.handleChangeText} 
-            value={this.state.text}>
-            onSubmitEditing={this.saveQuestion}
-          </TextInput>
-        </View>
-      )
-    }
     return (
-      <div>
-      </div>
+      <View>
+        <Text>Text</Text>
+        <TextInput 
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={this.handleChangeText} 
+          value={this.state.text}
+          onSubmitEditing={this.saveQuestion} />
+      </View>
     )
   }
 }
